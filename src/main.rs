1dfbef46197fe40753165e30722729990f9e86d2
Tiramister <nh111424@gmail.com>
2021-12-25 16:21:06 +0900

    debug

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+mod arms;
+mod csar;
+mod structures;
+
+use arms::Weights;
+use rand::Rng;
+use structures::uniform_matroid;
+
 fn main() {
-    println!("Hello, world!");
+    let n = 10_usize;
+    let rank = 5_usize;
+
+    let structure = uniform_matroid::Structure::new(n, rank);
+
+    let mut rng = rand::thread_rng();
+    let weights: Weights = (0..n).map(|_| rng.gen()).collect();
+
+    println!("{:?}", weights);
+
+    let naive_arm = csar::naive_maxgap(&structure, &weights);
+    let faster_arm = uniform_matroid::fast_maxgap(&structure, &weights);
+
+    println!("naive: {}, faster: {}", naive_arm, faster_arm);
 }
