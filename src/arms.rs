69f21bc8d585bab1b6d3d225dd83b97a3dddb5f5
Tiramister <nh111424@gmail.com>
2021-12-27 16:44:34 +0900

    Cease to create rng on every sampling

--- src/arms.rs
+++ src/arms.rs
@@ -1,17 +1,20 @@
+use rand::prelude::ThreadRng;
 use rand_distr::{Distribution, Normal};
 
 pub struct Arm {
     distr: Normal<f64>,
+    rng: ThreadRng,
 }
 
 impl Arm {
     pub fn new(mean: f64, std_dev: f64) -> Self {
         Arm {
             distr: Normal::new(mean, std_dev).unwrap(),
+            rng: rand::thread_rng(),
         }
     }
-    pub fn sample(&self) -> f64 {
-        self.distr.sample(&mut rand::thread_rng())
+    pub fn sample(&mut self) -> f64 {
+        self.distr.sample(&mut self.rng)
     }
 
     pub fn get_mean(&self) -> f64 {
@@ -32,11 +35,11 @@ impl Arms {
         self.arms.push(Arm::new(mean, std_dev))
     }
 
-    pub fn sample(&self, i: usize) -> f64 {
+    pub fn sample(&mut self, i: usize) -> f64 {
         self.arms[i].sample()
     }
 
-    pub fn get_mean(&self, i: usize) -> f64 {
+    pub fn get_mean(&mut self, i: usize) -> f64 {
         self.arms[i].sample()
     }
 }
