79a010a65a20b2b1ef2f11079452bf76f851e44a
Tiramister <nh111424@gmail.com>
2021-12-27 16:45:04 +0900

    Implement Sampler and CSAR algorithm

--- src/csar.rs
+++ src/csar.rs
@@ -1,5 +1,43 @@
-use crate::arms::Weights;
+use crate::arms::{Arms, Weights};
+use crate::sampler::Sampler;
 use crate::structures::Structure;
+
+pub fn csar(mut structure: impl Structure, arms: &mut Arms) -> Vec<usize> {
+    let mut accepted_arms = Vec::<usize>::new();
+
+    // the total number of arms
+    let n = structure.get_indices().len();
+
+    let mut samplers: Vec<Sampler> = (0..n).map(|_| Sampler::new()).collect();
+
+    for _ in 0..n {
+        // sample the remaining arms 1000 times
+        for &i in structure.get_indices() {
+            for _ in 0..1000 {
+                samplers[i].observe(arms.sample(i));
+            }
+        }
+
+        let weights: Vec<f64> = (0..n).map(|i| samplers[i].get_mean()).collect();
+
+        let best_arms = structure.optimal(&weights);
+        let maxgap_arm = structure.fast_maxgap(&weights);
+
+        if best_arms.contains(&maxgap_arm) {
+            accepted_arms.push(maxgap_arm);
+            structure.contract_arm(maxgap_arm);
+        } else {
+            structure.delete_arm(maxgap_arm);
+        }
+    }
+
+    {
+        let weights: Vec<f64> = (0..n).map(|i| samplers[i].get_mean()).collect();
+        println!("empirical means: {:?}", weights);
+    }
+
+    accepted_arms
+}
 
 pub fn naive_maxgap(structure: &impl Structure, weights: &Weights) -> usize {
     let opt_arms = structure.optimal(weights);
---
+++ src/sampler.rs
@@ -0,0 +1,30 @@
+pub struct Sampler {
+    trial: u32,
+    mean: f64,
+}
+
+impl Sampler {
+    pub fn new() -> Self {
+        Sampler {
+            trial: 0_u32,
+            mean: 0_f64,
+        }
+    }
+
+    /**
+     * Update the empirical mean by the observed value.
+     */
+    pub fn observe(&mut self, val: f64) -> &mut Self {
+        self.mean = (self.mean * (self.trial as f64) + val) / ((self.trial + 1) as f64);
+        self.trial += 1;
+        self
+    }
+
+    pub fn get_mean(&self) -> f64 {
+        self.mean
+    }
+
+    pub fn get_trial(&self) -> u32 {
+        self.trial
+    }
+}
